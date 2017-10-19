472e9c48f7a633bbfb354382ee4b9d478caec6ee
José Tissei <jose.tissei@benner.com.br>
2017-10-18 22:28:21 -0200

    Removendo Codigo Temporariamente

--- Semana_15_Strategy/UmDesignPatternPorSemana.Strategy/UmDesignPatternPorSemana.Strategy/FreteDaFelicidade/FreteStrategy/IFrete.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace UmDesignPatternPorSemana.Strategy.FreteDaFelicidade.FreteStrategy
-{
-    public interface IFrete
-    {
-        decimal CalcularCusto(int distancia);
-    }
-}
