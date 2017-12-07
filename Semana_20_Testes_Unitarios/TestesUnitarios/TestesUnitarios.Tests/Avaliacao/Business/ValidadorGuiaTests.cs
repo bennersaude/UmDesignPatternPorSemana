788c79deffbbb50e7f0fd6740c694257c8036e47
Fabio Cezar Baía <fabio.baia@benner.com.br>
2017-12-07 10:22:41 -0200

    Estrutura inicial validadorGuiaTests

---
+++ Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios.Tests/Avaliacao/Business/ValidadorGuiaTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace TestesUnitarios.Tests.Avaliacao.Business
+{
+    [TestFixture]
+    public class ValidadorGuiaTests
+    {
+        //todo nao deve permitir guia com data atendimento maior que hoje
+        [Test]
+        public void DeveValidarDataAtendimentoMaiorQueHoje()
+        {
+            
+        }
+
+        //todo deve preencher NumeroDeclaracaoObito qdo indicadorObito for true
+
+        //
+    }
+}
