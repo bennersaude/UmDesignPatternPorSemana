79cb7ce624bbe2d2b2d30f4149eb41132aabd426
Fabio Cezar Baía <fabio.baia@benner.com.br>
2017-12-07 09:46:26 -0200

    Extraindo metodo para classe fake

---
+++ Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios.Tests/Avaliacao/Business/GuiaFake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TestesUnitarios.Avaliacao.Entidades;
+
+namespace TestesUnitarios.Tests.Avaliacao.Business
+{
+    public class GuiaFake
+    {
+        public Guia ObterGuiaValida()
+        {
+            return new Guia()
+            {
+                DataAtendimento = DateTime.Now,
+                IndicadorDeclaracaoObitoRn = false,
+                NumeroDeclaracaoObito = "123"
+            };
+        }
+    }
+}
--- Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios.Tests/Avaliacao/Business/ProcessamentoGuiasTests.cs
+++ Semana_20_Testes_Unitarios/TestesUnitarios/TestesUnitarios.Tests/Avaliacao/Business/ProcessamentoGuiasTests.cs
@@ -1,8 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
-using System;
 using TestesUnitarios.Avaliacao.Business;
-using TestesUnitarios.Avaliacao.Entidades;
 
 namespace TestesUnitarios.Tests.Avaliacao.Business
 {
@@ -14,22 +12,11 @@ namespace TestesUnitarios.Tests.Avaliacao.Business
         {
             var processamentoGuia = new ProcessamentoGuias();
 
-            var guia = ObterGuiaFake();
+            var guia = new GuiaFake().ObterGuiaValida();
 
             var resultado = processamentoGuia.Processar(guia);
 
             resultado.ShouldBeEquivalentTo(new RespostaProcessamentoDtoFake().ObterRespostaComSucesso());
-
-        }
-
-        private Guia ObterGuiaFake()
-        {
-            return new Guia()
-            {
-                DataAtendimento = DateTime.Now,
-                IndicadorDeclaracaoObitoRn = false,
-                NumeroDeclaracaoObito = "123"
-            };
         }
     }
 }
